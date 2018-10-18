3d8f3e183216755d61a61183aaebbabf4592ccc4
Herve Mbilo <herve.mbilo@gmail.com>
2018-10-18 22:00:42 +0200

    18-10-2018

---
+++ PRM2/src/Labo02/Exercice06.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Labo02;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author herve
+ */
+public class Exercice06 {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        
+        System.out.print("Type a number between 1 and 10 : ");
+        int userNumber = scan.nextInt();
+        
+        if(userNumber >= 0 && userNumber <= 10 ){
+            System.out.println("You are in the range");
+        } else {
+           System.out.println("you are not in the range");
+                   
+        }
+    }
+}
