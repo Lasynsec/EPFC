8d9ef61858b996e11dea6bf91324792622d35384
Herve Mbilo <herve.mbilo@gmail.com>
2018-11-09 20:09:52 +0100

    09-11-2018 Home-DT

--- PRM2/src/Labo03/Exercice07.java
+++ PRM2/src/Labo03/Exercice07.java
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
@@ -40,37 +39,3 @@ public class Exercice07 {
             System.out.println("The last value is " + lastValue);
     }
 }
-=======
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-package Labo03;
-
-import java.util.Scanner;
-
-/**
- *
- * @author herve
- */
-public class Exercice07 {
-    public static void main(String[] args) {
-        Scanner scan = new Scanner(System.in);
-        
-        System.out.println("Please type a number greater than 0");
-        int userNumber = scan.nextInt();
-        
-        while(userNumber <= 0){
-            
-            if(userNumber <= 0){
-                System.out.println("Please type a number greater than 0");
-            }
-            System.out.println("Please type a number greater than 0");
-            userNumber = scan.nextInt();
-        }
-        
-        
-    }
-}
->>>>>>> 0847149555d669c8c19841e612b695cc70277c20
