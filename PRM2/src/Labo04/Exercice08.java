c213d76d3abfa25cacecbbd4f77cf656f2302d6d
Herve Mbilo <herve.mbilo@gmail.com>
2018-11-11 15:53:13 +0100

    09-11-2018 Home-DT

--- PRM2/src/Labo04/Exercice08.java
+++ PRM2/src/Labo04/Exercice08.java
@@ -15,8 +15,38 @@ public class Exercice08 {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
         
-        System.out.println("Please type the ");
+        System.out.print("Please type a number for the base : ");
+        int base = scan.nextInt();
         
+        System.out.print("Please type a number for the exponent : ");
+        int exponent = scan.nextInt();
         
+        while(base == 0  && exponent == 0){
+            System.out.println("The base and the exponent must not be equal to 0");
+            
+            System.out.print("Please type a number for the base : ");
+            base = scan.nextInt();
+        
+            System.out.print("Please type a number for the exponent : ");
+            exponent = scan.nextInt();
+        }
+        
+        boolean isNegative = false;
+        
+        if(exponent < 0){
+            exponent = -exponent;
+            isNegative = true;
+        }
+        double exponential = 1;
+ 
+        for(int i = 0; i < exponent; ++i){
+            exponential *= base; 
+        }    
+        System.out.println("exponential : " + exponential);
+        
+        if(isNegative)
+            exponential = (double) 1/exponential;
+
+        System.out.println(base + "^" + exponent + " = " + exponential);
     }
 }
