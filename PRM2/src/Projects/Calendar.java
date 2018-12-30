141d124bd0bf4db0ba5783219808c775de56f7b6
Herve Mbilo <herve.mbilo@gmail.com>
2018-12-30 21:36:54 +0100

    Merge origin/master
    Conflicts:
    	PRM2/nbproject/private/private.xml

--- PRM2/src/Projects/Calendar.java
+++ PRM2/src/Projects/Calendar.java
@@ -5,65 +5,175 @@
  */
 package Projects;
 
+import java.util.Scanner;
+import seqint.SeqInt;
+import seqint.SeqIntIterator;
+
 /**
  *
  * @author herve
  */
 public class Calendar {
-    
+
     /**
-     * Return a integer corresponding to the day of the week (0 = monday, 1 = tuesday, 2 = wednesday,etc.) for a given date in parameter.
-     * The year in  parameter have to be in AAAA format(2008).
+     * Return a integer corresponding to the day of the week (0 = monday, 1 =
+     * tuesday, 2 = wednesday,etc.) for a given date in parameter. The year in
+     * parameter have to be in AAAA format(2008).
+     *
      * @param day
      * @param month
      * @param year
-     * @return 
+     * @return
      */
-    public static int dayOfWeek(int day, int month, int year){
-        int dayOfWeekNumber = 0;
+    public static int dayOfWeek(int day, int month, int year) {
+        double equationMonth = Math.round((double)((month+1)*13)/5);
+        int eqMonth = (int) equationMonth;
+        int dayOfWeekNumber = (day+eqMonth+ (year % 100) + ((year % 100)/4) + ((year/100)/4) + 5*(year/100)) % 7;
+            
         return dayOfWeekNumber;
     }
-    
+
     /**
      * Return the number of days in the given month in parameter.
+     *
      * @param month
      * @param year
-     * @return 
+     * @return
      */
-    public static int daysInMonth(int month, int year){
+    public static int daysInMonth(int month, int year) {
         int numberOfDaysInAMonth = 0;
+        if (month == 1 || month == 3 || month == 5 || month == 3 || month == 7 || month == 8 || month == 10 || month == 12) {
+            numberOfDaysInAMonth = 31;
+        } else if (month == 4 || month == 6 || month == 9 || month == 11) {
+            numberOfDaysInAMonth = 30;
+        } else {
+            if (isLeapYear(year)) {
+                numberOfDaysInAMonth = 29;
+            } else {
+                numberOfDaysInAMonth = 28;
+            }
+        }
+
         return numberOfDaysInAMonth;
     }
-    
+
     /**
-     * Receive a year un the AAAA format and return true if the year is leap otherwise false.
+     * Receive a year un the AAAA format and return true if the year is leap
+     * otherwise false.
+     *
      * @param year
-     * @return 
+     * @return
      */
-    public static boolean isLeapYear(int year){
-        boolean isLeap = true;
-        return true;
-    } 
-    
+    public static boolean isLeapYear(int year) {
+        boolean isLeap = false;
+        if (year % 4 == 0 && year % 100 != 0 || year % 400 == 0) {
+            isLeap = true;
+        }
+
+        return isLeap;
+    }
+
     /**
      * Display the number of the given day in parameter (JJ = 01).
-     * @param day 
+     *
+     * @param day
      */
-    public static void printDay(int day){
-        System.out.println("the day");
+    public static void printDay(int day) {
+        if(day < 32){            
+            if(day < 10){
+                System.out.print("0"+day+"   ");
+            }else {
+                System.out.print(day+"   ");
+            }
+        }
     }
-    
-    /**
-     * Display the calendar of the given month in parameter. Weeks start on Monday.
-     * @param month
-     * @param year 
-     */
-    public static void printCalendar(int month, int year){
-  
-    }
-    
-    public static void main(String[] args) {
+    public static int convertFromZellerDay(int startingDay){
+        int result = 0;
+        if(startingDay > 1 && startingDay < 7){
+            result = startingDay - 2;
+        } else if(startingDay == 0){
+            result = 5;
+        } else {
+            result = 6;
+        }
         
+        return result;
     }
-    
+
+    /**
+     * Display the calendar of the given month in parameter. Weeks start on
+     * Monday.
+     *
+     * @param month
+     * @param year
+     */
+    public static void printCalendar(int month, int year) {
+        String wordMonth = "January";
+        switch (month) {
+            case 2:
+                wordMonth = "February";
+                break;
+            case 3:
+                wordMonth = "March";
+                break;
+            case 4:
+                wordMonth = "April";
+                break;
+            case 5:
+                wordMonth = "May";
+                break;
+            case 6:
+                wordMonth = "June";
+                break;
+            case 7:
+                wordMonth = "july";
+                break;
+            case 8:
+                wordMonth = "August";
+                break;
+            case 9:
+                wordMonth = "September";
+                break;
+            case 10:
+                wordMonth = "October";
+                break;
+            case 11:
+                wordMonth = "November";
+                break;
+            case 12:
+                wordMonth = "December";
+        }
+
+        System.out.println("================================");
+        System.out.println(wordMonth + " " + year);
+        System.out.println("================================");
+        System.out.println("Mon  Tues Wed Thurs Fri  Sat Sun");
+        
+        int startingDay = dayOfWeek(1,month,year);
+        System.out.println(startingDay);
+        int count = 0;
+        int nbrOfWeekLine = 6;
+        
+        if(startingDay == 1 || startingDay == 0){
+            nbrOfWeekLine = 5;
+        }
+        for (int i = 0; i < daysInMonth(month, year)/nbrOfWeekLine; ++i) {
+            for (int j = 0; j < 7; ++j) {
+                if (j < convertFromZellerDay(startingDay) && i == 0) {
+                    System.out.print("     ");
+                } else {
+                    printDay(++count);
+                    //System.out.print("("+j+")");
+                }
+            }
+            System.out.println("");
+        }
+
+        System.out.println("================================");
+    }
+
+    public static void main(String[] args) {
+        printCalendar(7,2018);
+    }
+
 }
